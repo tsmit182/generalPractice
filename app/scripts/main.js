1126c37f3c0b9d5527211ecddef382fb5abe4764
Taylor Smith <tsmit182@gmail.com>
2014-08-09 12:51:10 -0400

    add some content from ytc repo. realize practice will be better achieved at ytc repo. keeping this repo open if needed in future

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('The Iron Yard Rocks');
+
+var commentList = [
+	{
+		id : 1,
+		comment : "Hello World"
+	},
+
+	{
+		id : 2,
+		comment: "Second comment"
+	},
+
+	{
+		id: 3,
+		comment: "Third!"
+	}
+];
+
+
+$(document).ready(function() {
+
+	$(".comment_submit").click(function(){
+		var commentValue = document.getElementById("comment_input").value;
+		$(".commentsRendered").prepend(commentList);
+	});
+});
